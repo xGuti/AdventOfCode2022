369ee1fe726837260664e9c2a4d4ca48779d47f7
Karol Hartwig <43039961+xGuti@users.noreply.github.com>
2022-12-01 22:22:04 +0100

    e1
    Done task #1

---
+++ AdventSolutions/AdventSolutions/DayOne.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventSolutions
+{
+    internal class DayOne
+    {
+        private string[] _text = System.IO.File.ReadAllLines(@"C:\Users\Guti\Documents\GitHub\AdventOfCode2022\AdventSolutions\AdventSolutions\input.txt");
+        private int maxCalories = 0;
+
+
+        public int CalculateMostValue()
+        {
+            int sumOfCalories = 0;
+
+            foreach(var line in _text)
+            {
+                if (line != "")
+                {
+                    try
+                    {
+                        sumOfCalories += Int32.Parse(line);
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine($"Unable to parse '{line}'");
+                    }
+                }
+                else
+                {
+                    if (sumOfCalories > maxCalories)
+                        maxCalories = sumOfCalories;
+                    sumOfCalories = 0;
+                }
+            }
+
+            return maxCalories;
+        }
+    }
+}
---
+++ AdventSolutions/AdventSolutions/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AdventSolutions;
+
+Console.WriteLine("Hello, World!");
+DayOne dayOne = new DayOne();
+Console.WriteLine($"Max; {dayOne.CalculateMostValue()}");
